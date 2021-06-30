2ad845d38aa759bbbb5c0a411d768c3f6faf1b59
frymn <francismotanya@outlook.com>
2021-06-30 16:54:18 +0300

    DB UPDATE

--- church/settings.py
+++ church/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'church.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'church',
-            'USER': 'postgres',
-            'PASSWORD': 'Admin',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'church',
+#             'USER': 'postgres',
+#             'PASSWORD': 'Admin',
+#             'HOST': 'localhost',
+#             'PORT': '',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
