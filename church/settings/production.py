96c046ef30341e16072973c908ec421aec0dad20
frymn <francismotanya@outlook.com>
2021-08-13 11:30:21 +0300

    production

--- church/settings/production.py
+++ church/settings/production.py
@@ -43,7 +43,7 @@ DATABASES = {
 
 
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET']
 
 ALLOWED_HOSTS = ['https://frymn-church.herokuapp.com']
 
